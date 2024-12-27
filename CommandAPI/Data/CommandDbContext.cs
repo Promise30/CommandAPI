7f8550b993790bb66a220fc32dbf9a0d3dd442dc
Promise30 <promiseadeagbo@gmail.com>
2024-12-27 21:37:11 +0100

    Added Command Model to API project

---
+++ CommandAPI/Data/CommandDbContext.cs
@@ -0,0 +1,21 @@
+﻿using CommandAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CommandAPI.Data
+{
+    public class CommandDbContext : DbContext
+    {
+        public CommandDbContext(DbContextOptions options) : base(options)
+        {
+        }
+        public DbSet<Command> Commands => Set<Command>();
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Command>().HasKey(c=> c.Id);
+            modelBuilder.Entity<Command>().Property(c=> c.HowTo).IsRequired().HasMaxLength(250);
+            modelBuilder.Entity<Command>().Property(c => c.Platform).IsRequired();
+            modelBuilder.Entity<Command>().Property(c => c.CommandLine).IsRequired();
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
