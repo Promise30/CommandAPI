bfaf2292b5cd835324d35848658e1c63430fdf1f
Promise30 <promiseadeagbo@gmail.com>
2024-12-29 18:31:53 +0100

    Initial project config for database setup, mapping and service registration

--- CommandAPI/Program.cs
+++ CommandAPI/Program.cs
@@ -1,12 +1,22 @@
+using CommandAPI.Data;
+using CommandAPI.Repos;
+using Microsoft.EntityFrameworkCore;
+using Npgsql;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
+var conn = new NpgsqlConnectionStringBuilder();
+conn.ConnectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+conn.Username = builder.Configuration["UserID"];
+conn.Password = builder.Configuration["Password"];
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
+builder.Services.AddDbContext<CommandDbContext>(opt => opt.UseNpgsql(conn.ConnectionString));
+builder.Services.AddScoped<ICommandRepo, CommandRepo>();
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
