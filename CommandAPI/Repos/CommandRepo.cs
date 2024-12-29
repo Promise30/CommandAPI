0de33e30e9db330942646303df7e92952d5b2fe8
Promise30 <promiseadeagbo@gmail.com>
2024-12-29 18:30:54 +0100

    API project database repository setup

--- CommandAPI/Repos/CommandRepo.cs
+++ CommandAPI/Repos/CommandRepo.cs
@@ -1,6 +1,43 @@
-﻿namespace CommandAPI.Repos
+﻿using CommandAPI.Data;
+using CommandAPI.Models;
+
+namespace CommandAPI.Repos
 {
     public class CommandRepo : ICommandRepo
     {
+        private readonly CommandDbContext _context;
+        public CommandRepo(CommandDbContext context)
+        {
+            _context = context;
+        }
+        public void CreateCommand(Command cmd)
+        {
+            _context.Commands.Add(cmd);
+        }
+
+        public void DeleteCommand(Command cmd)
+        {
+            _context.Commands.Remove(cmd);
+        }
+
+        public IEnumerable<Command> GetAllCommands()
+        {
+            return _context.Commands.ToList();
+        }
+
+        public Command GetCommandById(int id)
+        {
+            return _context.Commands.FirstOrDefault(c=> c.Id == id);    
+        }
+
+        public bool SaveChanges()
+        {
+            return (_context.SaveChanges() >= 0);
+        }
+
+        public void UpdateCommand(Command cmd)
+        {
+            _context.Commands.Update(cmd);
+        }
     }
 }
--- CommandAPI/Repos/ICommandRepo.cs
+++ CommandAPI/Repos/ICommandRepo.cs
@@ -1,6 +1,14 @@
-﻿namespace CommandAPI.Repos
+﻿using CommandAPI.Models;
+
+namespace CommandAPI.Repos
 {
     public interface ICommandRepo
     {
+        bool SaveChanges();
+        IEnumerable<Command> GetAllCommands();
+        Command GetCommandById (int id);
+        void CreateCommand(Command cmd);
+        void UpdateCommand(Command cmd);
+        void DeleteCommand(Command cmd);
     }
 }
