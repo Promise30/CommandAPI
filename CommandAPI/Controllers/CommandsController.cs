ea97c0c14246144444b40e30966eb2ed1b442efe
Promise30 <promiseadeagbo@gmail.com>
2024-12-31 08:49:46 +0100

    Updated controller to use service methods

--- CommandAPI/Controllers/CommandsController.cs
+++ CommandAPI/Controllers/CommandsController.cs
@@ -2,6 +2,7 @@
 using CommandAPI.Dtos;
 using CommandAPI.Models;
 using CommandAPI.Repos;
+using CommandAPI.Services;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -11,26 +12,27 @@ namespace CommandAPI.Controllers
     [ApiController]
     public class CommandsController : ControllerBase
     {
-        private readonly ICommandRepo _repository;
-        private readonly IMapper _mapper;
-        public CommandsController(ICommandRepo repository, IMapper mapper)
+        //private readonly ICommandRepo _repository;
+        private readonly ICommandService _commandService;
+        //private readonly IMapper _mapper;
+        public CommandsController(ICommandService commandService)
         {
-            _repository = repository;
-            _mapper = mapper;
+            _commandService = commandService;
+            //_mapper = mapper;
         }
         [HttpGet]
         public ActionResult<IEnumerable<Command>> GetAllCommands()
         {
-            var commands = _repository.GetAllCommands();
-            return Ok(_mapper.Map<IEnumerable<CommandReadDto>>(commands));
+            var commands = _commandService.GetCommands();
+            return Ok(commands);
         }
         [HttpGet("{id:int}", Name = "GetCommand")]
         public ActionResult<Command> GetCommandById(int id)
         {
-            var command = _repository.GetCommandById(id);
+            var command = _commandService.GetCommandById(id);
             if (command == null)
                 return NotFound();
-            return Ok(_mapper.Map<CommandReadDto>(command));
+            return Ok(command);
         }
         [HttpPost]
         public ActionResult<Command> CreateCommand(CommandCreateDto commandCreateDto)
@@ -39,33 +41,28 @@ namespace CommandAPI.Controllers
             {
                 return BadRequest(ModelState);
             }
-            var commandModel = _mapper.Map<Command>(commandCreateDto);
-            _repository.CreateCommand(commandModel);
-            _repository.SaveChanges();
-
-            var commandReadDto = _mapper.Map<CommandReadDto>(commandModel);
-            return CreatedAtAction(nameof(GetCommandById), new {id= commandReadDto.Id}, commandReadDto);
+            _commandService.CreateCommand(commandCreateDto);
+            return NoContent();
+          
         }
         [HttpPut("{id:int}")]
         public ActionResult UpdateCommand(int id, CommandUpdateDto commandUpdateDto)
         {
-            var commandModelFromRepo = _repository.GetCommandById(id);
-            if (commandModelFromRepo == null)
-                return NotFound();
-            _mapper.Map(commandUpdateDto, commandModelFromRepo);
-            _repository.UpdateCommand(commandModelFromRepo);
-            _repository.SaveChanges();
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(commandUpdateDto);
+            }
+
+            _commandService.UpdateCommand(id, commandUpdateDto);
+           
             return NoContent();
         }
         [HttpDelete("{id:int}")]
         public ActionResult DeleteCommand(int id)
         {
-            var commandModelFromRepo = _repository.GetCommandById(id);
-            if (commandModelFromRepo == null)
-                return NotFound();
-            _repository.DeleteCommand(commandModelFromRepo);
-            _repository.SaveChanges();
+            _commandService.DeleteCommand(id);
             return NoContent();
         }
+        
     }
 }
