06d8f736671d356b6afcd9274ffb3f9016c6c47f
Promise30 <promiseadeagbo@gmail.com>
2024-12-31 08:49:59 +0100

    API project command service methods

---
+++ CommandAPI/Services/CommandService.cs
@@ -0,0 +1,60 @@
+﻿using AutoMapper;
+using CommandAPI.Dtos;
+using CommandAPI.Models;
+using CommandAPI.Repos;
+
+namespace CommandAPI.Services
+{
+    public class CommandService : ICommandService
+    {
+        private readonly ICommandRepo _commandRepository;
+        private readonly IMapper _mapper;
+
+        public CommandService(ICommandRepo commandRepository, IMapper mapper)
+        {
+            _commandRepository = commandRepository;
+            _mapper = mapper;
+        }
+
+        public void CreateCommand(CommandCreateDto commandReadDto)
+        {
+            if (commandReadDto == null)
+                throw new ArgumentException("Command not found");
+            var commandToCreate = _mapper.Map<Command>(commandReadDto);
+            _commandRepository.CreateCommand(commandToCreate);
+            _commandRepository.SaveChanges();   
+        }
+        public void DeleteCommand(int id)
+        {
+            var command = _commandRepository.GetCommandById(id);
+            if (command == null)
+                throw new ArgumentException("Command not found");
+            _commandRepository.DeleteCommand(command);
+            _commandRepository.SaveChanges();
+        }
+
+        public CommandReadDto GetCommandById(int id)
+        {
+            var command = _commandRepository.GetCommandById(id);
+            if (command == null)
+                throw new ArgumentException("Command not found");
+            return _mapper.Map<CommandReadDto>(command);
+        }
+
+        public IEnumerable<CommandReadDto> GetCommands()
+        {
+            var commands = _commandRepository.GetAllCommands();
+            return _mapper.Map<IEnumerable<CommandReadDto>>(commands);
+        }
+
+        public void UpdateCommand(int id, CommandUpdateDto command)
+        {
+            var commandModelFromRepo = _commandRepository.GetCommandById(id);
+            if (commandModelFromRepo == null)
+                throw new ArgumentException("Command not found");
+            _mapper.Map(command, commandModelFromRepo);
+            _commandRepository.UpdateCommand(commandModelFromRepo);
+            _commandRepository.SaveChanges();
+        }
+    }
+}
---
+++ CommandAPI/Services/ICommandService.cs
@@ -0,0 +1,15 @@
+﻿using CommandAPI.Dtos;
+using CommandAPI.Models;
+
+namespace CommandAPI.Services
+{
+    public interface ICommandService
+    {
+        IEnumerable<CommandReadDto> GetCommands();
+        CommandReadDto GetCommandById(int id);
+        void CreateCommand(CommandCreateDto commandReadDto);
+        void UpdateCommand(int id, CommandUpdateDto command);
+        void DeleteCommand(int id);
+
+    }
+}
